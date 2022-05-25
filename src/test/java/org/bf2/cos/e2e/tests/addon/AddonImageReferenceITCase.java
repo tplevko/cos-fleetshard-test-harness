7e7c88603c7958331a3eb36ab72015d5a8e3e885
Andrej Smigala <asmigala@users.noreply.github.com>
2022-05-25 11:20:04 +0200

    Add check that operator images use sha, not tag (#8)

---
+++ src/test/java/org/bf2/cos/e2e/tests/addon/AddonImageReferenceITCase.java
@@ -0,0 +1,36 @@
+package org.bf2.cos.e2e.tests.addon;
+
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.NamespacedOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+@Order(2)
+public class AddonImageReferenceITCase {
+
+    private static NamespacedOpenShiftClient client = new DefaultOpenShiftClient(new OpenShiftConfig(Config.autoConfigure(null)))
+            .inNamespace("redhat-openshift-connectors");
+
+    @DisplayName("image should have sha reference:")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("getAllDeployments")
+    public void imageShouldHaveShaReference(Deployment deployment) {
+        deployment.getSpec().getTemplate().getSpec().getContainers().forEach(c -> {
+            Assertions.assertTrue(c.getImage().contains("@sha256:"), () -> "no sha found in " + c.getImage());
+        });
+
+    }
+
+    static Stream<Deployment> getAllDeployments() {
+        return client.apps().deployments().list().getItems().stream();
+    }
+
+}
