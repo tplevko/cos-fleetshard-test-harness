2ddac0e795a9bb4147bd47810906eb52db437eea
Tomas Plevko <tplevko@redhat.com>
2022-06-08 16:07:38 +0200

    Add tag to image ref test (#13)

--- src/test/java/org/bf2/cos/e2e/tests/addon/AddonImageReferenceITCase.java
+++ src/test/java/org/bf2/cos/e2e/tests/addon/AddonImageReferenceITCase.java
@@ -8,12 +8,17 @@ import io.fabric8.openshift.client.OpenShiftConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
 @Order(2)
+@Tags(value = {
+        @Tag("install")
+})
 public class AddonImageReferenceITCase {
 
     private static NamespacedOpenShiftClient client = new DefaultOpenShiftClient(new OpenShiftConfig(Config.autoConfigure(null)))
