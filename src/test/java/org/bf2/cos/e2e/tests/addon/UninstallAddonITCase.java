f9aa8a22b207229d768d840249c5733a07202142
Tomas Plevko <tplevko@redhat.com>
2022-08-03 15:10:47 +0200

    MGDCTRS-1210 - check the deletion of observability ns (#20)

--- src/test/java/org/bf2/cos/e2e/tests/addon/UninstallAddonITCase.java
+++ src/test/java/org/bf2/cos/e2e/tests/addon/UninstallAddonITCase.java
@@ -6,9 +6,11 @@ import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftConfig;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -20,15 +22,20 @@ public class UninstallAddonITCase {
 
     private static DefaultOpenShiftClient client = new DefaultOpenShiftClient(new OpenShiftConfig(Config.autoConfigure(null)));
 
-    @Test
-    public void namespaceShouldHaveBeenDeleted() {
+    @DisplayName("Namespace should be deleted:")
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(strings = {
+            "redhat-openshift-connectors",
+            "redhat-openshift-connectors-observability",
+    })
+    public void namespaceShouldHaveBeenDeleted(String namespaceName) {
         Awaitility.await()
                 .atMost(Duration.ofMinutes(15))
                 .pollInterval(Duration.ofSeconds(15))
                 .pollDelay(Duration.ofSeconds(0))
                 .untilAsserted(() -> {
                             Optional<Namespace> namespace = client.namespaces().list().getItems().stream().filter(
-                                    n -> n.getMetadata().getName().equals("redhat-openshift-connectors")).findFirst();
+                                    n -> n.getMetadata().getName().equals(namespaceName)).findFirst();
                             Assertions.assertTrue(!namespace.isPresent(), () -> namespace + " namespace still present");
                         }
                 );
